412421c74a71121b328553b842a7694fda8e8ad7
RaymondKaruga <raymond.karuga@student.moringaschool.com>
2024-04-04 13:46:01 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const removeMain = document.querySelector('main#main')
+removeMain.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+
+newHeader.textContent = "Raymond is the champion"
